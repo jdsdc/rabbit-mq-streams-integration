8c77ade03769707b40b80d3e989052fa26684b79
Johannes Dressie <johannes.dressie@ecoclime.se>
2023-05-20 10:21:22 +0200

    First commit

---
+++ src/main/java/dev/coffe/springintegration/rabbitmqintegration/rabbitmqstreamsintegration/RabbitMqStreamsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package dev.coffe.springintegration.rabbitmqintegration.rabbitmqstreamsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqStreamsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqStreamsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/coffe/springintegration/rabbitmqintegration/rabbitmqstreamsintegration/RabbitMqStreamsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.coffe.springintegration.rabbitmqintegration.rabbitmqstreamsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqStreamsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
